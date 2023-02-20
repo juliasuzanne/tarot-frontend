047cc4e2ee9a5a3c78b4f9b49ee2fa065dc1429b
Julia Grimes <julia.s.grimes@gmail.com>
2023-02-19 19:57:32 -0500

    adjust variables

--- Tarots/tarot-frontend/src/CardsIndex.jsx
+++ Tarots/tarot-frontend/src/CardsIndex.jsx
@@ -4,9 +4,12 @@ import { useEffect } from "react";
 
 export function CardsIndex() {
   let [cards, setCards] = useState([]);
-  let [cardnum, setCardNum] = useState(1);
-
+  let [cardnum1, setCardNum1] = useState(1);
   let [randomCard1, setRandomCard1] = useState(1);
+  let [cardnum2, setCardNum2] = useState(2);
+  let [randomCard2, setRandomCard2] = useState(2);
+  let [cardnum3, setCardNum3] = useState(3);
+  let [randomCard3, setRandomCard3] = useState(3);
 
   const handleCards = () => {
     axios
@@ -20,10 +23,14 @@ export function CardsIndex() {
       });
   };
 
-  const handleGenerateNumbers = () => {
-    setCardNum(Math.ceil(Math.random() * 78));
+  const handleGenerateNumber1 = () => {
+    setCardNum1(Math.ceil(Math.random() * 78));
+    do {
+      setCardNum1(Math.ceil(Math.random() * 78));
+    } while (cardnum2 === cardnum1 || cardnum3 === cardnum1);
+
     axios
-      .get(`http://localhost:3000/cards/${cardnum}`)
+      .get(`http://localhost:3000/cards/${cardnum1}`)
       .then((response) => {
         console.log(response);
         setRandomCard1(response.data);
@@ -33,12 +40,50 @@ export function CardsIndex() {
       });
   };
 
-  useEffect(handleCards, []);
+  const handleGenerateNumber2 = () => {
+    setCardNum2(Math.ceil(Math.random() * 78));
+
+    do {
+      setCardNum2(Math.ceil(Math.random() * 78));
+    } while (cardnum2 === cardnum1 || cardnum3 === cardnum2);
+
+    axios
+      .get(`http://localhost:3000/cards/${cardnum2}`)
+      .then((response) => {
+        console.log(response);
+        setRandomCard2(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  const handleGenerateNumber3 = () => {
+    setCardNum3(Math.ceil(Math.random() * 78));
+    do {
+      setCardNum3(Math.ceil(Math.random() * 78));
+      console.log(cardnum2);
+    } while (cardnum2 === cardnum3 || cardnum3 === cardnum1);
+
+    axios
+      .get(`http://localhost:3000/cards/${cardnum3}`)
+      .then((response) => {
+        console.log(response);
+        setRandomCard3(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
 
   return (
     <div>
-      <button onClick={handleGenerateNumbers}> random number</button>
+      <button onClick={handleGenerateNumber1}> random number</button>
       <p> {randomCard1.name}</p>
+      <button onClick={handleGenerateNumber2}> random number</button>
+      <p> {randomCard2.name}</p>
+      <button onClick={handleGenerateNumber3}> random number</button>
+      <p> {randomCard3.name}</p>
       {cards.map((card) => (
         <div key={card.id}>
           <h2>{card.name}</h2>
